d8b1ff9d07ab2a2856934f96da945a24b48e1829
IsabellaM <a01663612@tec.mx>
2025-11-01 14:54:08 -0600

    Agregué funciones.py

---
+++ funciones.py
@@ -0,0 +1,18 @@
+import math
+
+def triangulo(b,a):
+	area = (b * a)/ 2
+	return area
+
+def rectangulo(b,a):
+        area = (b * a)
+        return area
+	
+def cuadrado(b):
+        area = (b * b)
+        return area
+
+def circulo(r):
+        area = math.pi * (r ** 2)
+        return area
+
