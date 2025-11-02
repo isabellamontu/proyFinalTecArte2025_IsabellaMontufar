71578c7926e49ca6506121fb4c8bfbfedbae3df0
IsabellaM <a01663612@tec.mx>
2025-11-02 00:05:18 -0600

    modifique el archivo de figuras principal para incluir areas

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from funciones import triangulo, circulo, rectangulo
 
 dataFile = pd.read_csv("figuras.csv")
 
@@ -9,4 +10,10 @@ perimetros = []
 
 for index, row in dataFile.iterrows():
 	print(f"Fila {index} : FIGURA= {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 ={row['MEDIDA2']}")
-
+	if row['FIGURA'] == "t":
+		area = triangulo(row['MEDIDA1'], row['MEDIDA2'])
+	elif row['FIGURA'] == "c":
+                area = circulo(row['MEDIDA1'])
+	elif row['FIGURA'] == "r":
+                area = rectangulo(row['MEDIDA1'], row['MEDIDA2'])
+	print(f"Area = {area} ")
